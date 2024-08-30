96b8e7d62904ac74b6ece4c4d563745e27d12f90
zjmuye <107024249+zjmuye@users.noreply.github.com>
2024-08-30 12:29:18 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["op.ns.lv.eu.org"];
-pub const PUBLIC_RS_PUB_KEY: &str = "faDOOI3h5AkEh1OolLBFwbtnfokBKVqXmA4xM12QI+U=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["XXXXXX"];
+pub const PUBLIC_RS_PUB_KEY: &str = "XXXXXX";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
